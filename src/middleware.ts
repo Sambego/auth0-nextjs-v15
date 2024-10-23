86d2e3596d75b163b4550029944f6addcf3f3c5e
Sambego <sambellen@gmail.com>
2024-10-23 14:47:53 +0200

    Initial commit

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { auth0Client } from "./lib/auth";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export async function middleware(request: any) {
+  return await auth0Client.middleware(request);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
